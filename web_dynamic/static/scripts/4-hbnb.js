4a9f7a5109bfa246a01461af9eb2e176e56e2fa8
Hardbass1991 <midailsorridente@gmail.com>
2022-12-23 15:23:09 -0500

    added templates for post request

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,81 +1,43 @@
-let selected = [];
 $( document ).ready(function() {
-  /* ACA COMIENZA LA 2*/
-  $(".amenities .popover li").on("input", function() {
-    let id = $(this).attr("data-id");
-    if ($(this).is(":checked")) {
-      selected.append(id);
+  let amen = {};
+  $('input[type="checkbox"]').change(function () {
+  if (this.checked) {
+    amen[$(this).data('id')] = ($(this).data('name'));
+  } else {
+    delete amen[($(this).data('id'))];
+  }
+  $('#h4_amenities').text(Object.keys(amen).map(function(k){return amen[k]}).join(", "));
+  });
+
+  $.get("http://127.0.0.1:5001/api/v1/status/", function(data) {
+    if (data.status === "OK") {
+      $("div#api_status").addClass("available");
     } else {
-      if selected.includes(id) {
-        let index = selected.indexOf(id);
-        array.splice(index, 1);
-      }
+      $("div#api_status").removeClass("available");
     }
   });
-  $(".amenities h4").text(selected.join(", "));
-  /*------2------*/
-  /*ACA COMIENZA LA 3*/
+
   $.ajax({
-    url: "http://0.0.0.0:5001/api/v1/status/",
-    type: "GET",
-    .done(function() {
-      $("div#api_status").addClass("available");
-    }),
-    .fail(function () {
-      $("div#api_status").removeClass("available");
-    })
-  })
-  /*------3------*/
-  /*ACA COMIENZA LA 4*/
-  $.ajax({
-    url: "http://0.0.0.0:5001/api/v1/places_search/",
     type: "POST",
+    url: "http://127.0.0.1:5001/api/v1/places_search/",
+    data: JSON.stringify({}),
     contentType: "application/json",
-    data: {},
-    .done(function (json) {
-      $.each(json, function(item) {
-        let article = $("<article></article>");
-        /*Article tag with three elements. The first one is 'title_box'*/
-        let title_box = $("<div></div>");
-        title_box.addClass("title_box");
-        let title_box_1 = $("<h2></h2>");
-        title_box_1.text(item.name);
-        let title_box_2 = $("<div></div>");
-        title_box_2.addClass("price_by_night");
-        title_box_2.text(item.price_by_night);
-        title_box.append(title_box_1, title_box_2);
-        /*Second part is 'information'*/
-        let info = $("<div></div>");
-        info.addClass("information");
-        let information_1 = $("<div></div>");
-        information_1.addClass("max_guest");
-        information_1.text(item.max_guest + "guests");
-        let information_2 = $("<div></div>");
-        information_2.addClass("number_rooms");
-        information_2.text(item.number_rooms + "rooms");
-        let information_3 = $("<div></div>");
-        information_3.addClass("number_bathrooms");
-        information_3.text(item.max_guest + "bathroomss");
-        info.append(information_1, information_2, information_3);
-        /*Finally, the description*/
-        let desc = $("<div></div>");
-        desc.addClass("description");
-        desc.text(item.description);
-        /*We append the three elements to article, and article itself to places*/
-        article.append(title_box, info, desc);
-        $(".places").append(article);
-      });
-    })
-  })
-  /*------4------*/
-  /*ACA COMIENZA LA 5*/
-  
-  $.ajax({
-    url: "http://0.0.0.0:5001/api/v1/places_search/",
-    type: "POST",
-    contentType: "application/json",
-    type: "POST",
-    data: {"amenities": selected}
-  })
-  /*------5------*/
+    success: function(data) {
+      console.log(data);
+    }
+  });
+  /*
+  $.post("http://127.0.0.1/api/v1/places_search",
+	  function(data, textStatus, jqXHR) {
+            console.log(data);
+	  });
+  */
+  $("button").on("click", function() {
+    $.post("http://127.0.0.1:5001/api/v1/places_search",
+          JSON.stringify(amen),
+	  function(data, textStatus, jqXHR) {
+            console.log(data)
+	  }
+    )
+  });
 });
