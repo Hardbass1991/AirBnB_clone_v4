8926bc642d4a7ceecc8c0c43145c321f43dc4f33
Hardbass1991 <midailsorridente@gmail.com>
2022-12-23 09:47:56 -0500

    Added script for amenity checking

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  let amen = {};
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      amen[$(this).data('id')] = ($(this).data('name'));
+    } else {
+      delete amen[($(this).data('id'))];
+    }
+    $('#h4_amenities').text(Object.keys(amen).map(function(k){return amen[k]}).join(", "));
+  });
+});
