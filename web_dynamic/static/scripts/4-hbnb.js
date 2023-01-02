56924e3548572607fbef45878a94126cd8295f7e
Hardbass1991 <midailsorridente@gmail.com>
2023-01-02 11:48:23 -0500

    added search button feature that filters results

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -59,15 +59,45 @@ $( document ).ready(function() {
       });
     }
   });
-
+  /* ---------5-------*/
   $("button").on("click", function() {
+    let amenities = {}
+    amenities["amenities"] = Object.keys(amen);
+    console.log(amen);
+    console.log(Object.keys(amen));
     $.ajax({
       type: "POST",
       url: "http://127.0.0.1:5001/api/v1/places_search",
-      data: JSON.stringify(amen),
+      data: JSON.stringify(amenities),
       contentType: "application/json",
       success: function(data) {
-        console.log("GAAAAAA");
+        $("section.places").empty();
+        $.each(data, function(index, value) {
+          $("section.places").append(`
+	  <article>
+	    <div class="title_box">
+	      <h2>${value.name}</h2>
+	      <div class="price_by_night">
+	        $${value.price_by_night}
+	      </div>
+	    </div>
+	    <div class="information">
+	      <div class="max_guest">
+	        ${value.max_guest} Guest${value.max_guest !== 1 ? 's' : ''}
+	      </div>
+	      <div class="number_rooms">
+	        ${value.number_rooms} Bedroom${value.number_rooms !== 1 ? 's' : ''}
+	      </div>
+	      <div class="number_bathrooms">
+	        ${value.number_bathrooms} Bathroom${value.number_bathrooms !== 1 ? 's' : ''}
+	      </div>
+	    </div>
+	    <div class="description">
+	      ${value.description.replace(/<BR ?\/?>/g, "\n")}
+	    </div>
+	  </article>
+	  `);
+        });
       }
     });
   });
